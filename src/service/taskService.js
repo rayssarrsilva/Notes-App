9e75818e55272dd34ca285477bf65c29001ee29b
Rayssa <rayssarrsilva1@gmail.com>
2026-07-18 16:35:27 -0300

    Create the CRUD of the tasks

--- src/service/taskService.js
+++ src/service/taskService.js
@@ -1,3 +1,40 @@
-// functions to manipulate colections of tasks (search all tasks, filter by priority)
 // operations that is about all tasks
+import Task from "../models/task.js";
 
+export default class TaskService {
+    constructor() {
+        this.tasks = [];
+    }
+
+    setTask(task){
+        this.tasks.push(task);
+    }
+
+    getTaskList(){
+        return this.tasks;
+    }
+
+    setRemove(id){
+        const index = this.tasks.findIndex(task => task.id === id);
+        if (index !== -1){
+            this.tasks.splice(index, 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getTask(id){
+        return this.tasks.find(task => task.id === id);
+    }
+
+    updateTask(id, task){
+        const index = this.tasks.findIndex(task => task.id === id);
+        if (index !== -1){
+            Object.assign(this.tasks[index], task);
+            return this.tasks[index];
+        } else {
+            return null;
+        }
+    }
+}
